e696d2deb92221019b3e9f6213fb99020bd8c3b3
Atila Moretto <atilam@motorola.com>
2018-03-30 14:12:57 -0300

    Graficos para analise 2 - diferentes estacoes do ano

--- trabalho.R
+++ trabalho.R
@@ -1,8 +1,10 @@
 ########################################
 # Trabalho Final - INF-0612          
-# Nome(s): ??tila de Moura Tavano Moretto
-#          S??rgio Henrique Martini Marinello
+# Nome(s): Átila de Moura Tavano Moretto
+#          Sérgio Henrique Martini Marinello
 ########################################
+
+library(ggplot2)
 
 # Download do arquivo csv que cont??m os dados
 con <- url("https://www.ic.unicamp.br/~zanoni/cepagri/cepagri.csv")
@@ -73,4 +75,163 @@ names(dfRepeticoes) <- c("Número de repetições consecutivas", "Periodo de tem
 
 dfRepeticoes
 # Não houveram falhas que perduraram 48hs
+
+
+#confirmar se ha horarios duplicados
+# Remover as amostras que se repetem em 24 horas 
+########## REVER ESTE PERIODO ############
+cepagri <- cepagri[!(consecutive(cepagri$Temperatura, 144) &
+                       consecutive(cepagri$Vento, 144) &
+                       consecutive(cepagri$Umidade, 144) &
+                       consecutive(cepagri$Sensacao, 144)),]
+
+
+#########   Analise 2 - estacoes do ano ############
+
+# Calcula a média das medições realizadas ao longo dos 3 anos, agrupadas por cada dia de cada mês
+mediaPorDia <- aggregate(cepagri[,2:5],list(format(cepagri$Horario, "%m-%d")), mean)
+# Nomeia cada coluna do data frame mediaPorDia
+colnames(mediaPorDia) <- c("Data", "Temperatura", "Vento", "Umidade", "Sensacao")
+
+# Adiciona uma coluna no data frame que indica a qual estação aquele dia pertence
+mediaPorDia$Estacao <- ""
+# Os dias de Verão são aqueles compreendidos entre 21 de dezembro e 20 de março
+mediaPorDia[mediaPorDia$Data >= "12-21" | mediaPorDia$Data < "03-21",]$Estacao <- "Verao"
+# Os dias de Outono são aqueles compreendidos entre 21 de março e 20 de junho
+mediaPorDia[mediaPorDia$Data >= "03-21" & mediaPorDia$Data < "06-21",]$Estacao <- "Outono"
+# Os dias de Inverno são aqueles compreendidos entre 21 de Junho e 20 de Setembro
+mediaPorDia[mediaPorDia$Data >= "06-21" & mediaPorDia$Data < "09-21",]$Estacao <- "Inverno"
+# Os dias de Primavera são aqueles compreendidos entre 21 de Setembro e 20 de Dezembro
+mediaPorDia[mediaPorDia$Data >= "09-21" & mediaPorDia$Data < "12-21",]$Estacao <- "Primavera"
+
+# Adiciona uma coluna no data frame mediaPorDia que indica qual o ordinal de cada dia
+# dentro da estação ao qual ele pertence. Inicializa a coluna com valores "0"
+mediaPorDia$diaEstacao <- 0
+
+# A partir de agora vamos calcular os ordinais de cada dia de cada estação e popular
+# no data frame mediaPorDia.
+# O Verão precisa ter uma tratamento especial pois ele começa no final do ano anterior
+# e termina no começo do ano atual.
+# veraoInicial indica o ordinal do primeiro dia da estação Verão a partir do inicio do ano
+# A primeira atribuição a veraoInicial conta quantos dias de verão existem em dezembro
+veraoInicial <- length(mediaPorDia[mediaPorDia$Estacao ==  "Verao" & mediaPorDia$Data > "12-01",]$Data)
+# Esta segunda atribuição adiciona 1 para refletir o ordinal do 
+# primeiro dia de verão em Janeiro
+veraoInicial <- veraoInicial + 1
+# veraoFinal indica o ordinal do ultimo dia da estação verão
+veraoFinal <- length(mediaPorDia[mediaPorDia$Estacao ==  "Verao",]$Data)
+
+# O vetor "ordinal" contem o ordinal do primeiro dia de cada estação a começar a contar
+# do primeiro dia de janeiro
+ordinal <- c("Verao" = veraoInicial, "Outono" = 1, "Inverno" = 1, "Primavera" = 1)
+
+# Aqui populamos os ordinais de cada dia em sua respectiva estação do ano 
+# no data frame "mediaPorDia"
+for (i in mediaPorDia$Data) {
+  # estacao_i guarda a estação ao qual o dia atual pertence
+  estacao_i <- mediaPorDia[mediaPorDia$Data == i,]$Estacao
+  
+  # linha_i é um vetor de booleanos que será utilizado para indexar o data frame
+  # "mediaPorDia" e acessar a linha correspondente à iteração atual no "for"
+  linha_i <- mediaPorDia$Data == i
+  
+  # Atribuição do ordinal do dia em sua respectiva estação
+  mediaPorDia[linha_i,]$diaEstacao <- ordinal[[estacao_i]]
+  # Soma 1 ao ordinal da estação utilizada nesta iteração
+  ordinal[[estacao_i]] <- ordinal[[estacao_i]] + 1
+  
+  # Ao terminar de passar pelos dias de Verão do inicio do ano, chegamos ao 
+  # final dos ordinais que representam o Verão. Devemos reiniciar a variável
+  # dos ordinais do Verão para representar os primeiros ordinais da estação,
+  # que estão localizados no fim do ano.
+  if (estacao_i == "Verao" & ordinal[[estacao_i]] > veraoFinal) {
+    ordinal[[estacao_i]] <- 1
+  }
+}
+
+# No momento, as estações são tratadas como texto e, quando necessário, o R irá
+# ordená-las pelos seus caracteres. Para melhor visualição dos gráficos
+# vamos ordenar as estações de acordo com sua posição no ano
+# A ordem será: Verão, Outono, Inverno, Primavera
+estacaoFactor <- as.factor(mediaPorDia$Estacao)
+mediaPorDia$Estacao <- factor(estacaoFactor, levels(estacaoFactor)[c(4,2,1,3)])
+
+# gTemperatura representa o grafico das médias diárias de temperatura em cada
+# estação, levando em consideração os 3 anos com registro.
+# Alem disso, traçamos uma curva de ajuste com intervalo de confiança para
+# termos uma referência da tendendencia dos valores desenhados ao longo do período
+
+# Cria o grafico gTemperatura indicando que os dados deverão ser agrupados por Estacao
+# e que cada estação deverá ter uma cor distinta, além disso, o eixo X deverá contem os
+# valores dos ordinais de cada dia dentro de sua estação.
+gTemperatura <- ggplot(mediaPorDia, aes(x=diaEstacao, group = Estacao, colour = Estacao)) 
+# Adiciona uma linha conectando todos os pontos de temperatura média
+gTemperatura <- gTemperatura + geom_line(aes(y = Temperatura))
+# Adiciona uma curva de ajuste com intervalo de confiança para os valores de temperatura média
+gTemperatura <- gTemperatura + geom_smooth(aes(y = Temperatura))
+# Pede a separação do gráfico em imagens distintas, sendo uma para cada estação, alem disso
+# indica que as imagens devem estar concentradas em uma única linha.
+gTemperatura <- gTemperatura + facet_wrap(~ Estacao, nrow=1)
+# Exibe o gráfico
+gTemperatura
+
+
+# gUmidade representa o gráfico das médias diárias de umidade em cada
+# estação, levando em consideração os 3 anos com registro.
+# Alem disso, traçamos uma curva de ajuste com intervalo de confiança para
+# termos uma referência da tendendencia dos valores desenhados ao longo do periodo
+
+# Cria o gráfico gUmidade indicando que os dados deverão ser agrupados por Estacao
+# e que cada estação deverá ter uma cor distinta, além disso, o eixo X deverá contem os
+# valores dos ordinais de cada dia dentro de sua estação.
+gUmidade <- ggplot(mediaPorDia, aes(x=diaEstacao, group = Estacao, colour = Estacao)) 
+# Adiciona uma linha conectando todos os pontos de umidade média
+gUmidade <- gUmidade + geom_line(aes(y = Umidade))
+# Adiciona uma curva de ajuste com intervalo de confiança para os valores de umidade média
+gUmidade <- gUmidade + geom_smooth(aes(y = Umidade))
+# Pede a separação do gráfico em imagens distintas, sendo uma para cada estação, alem disso
+# indica que as imagens devem estar concentradas em uma unica linha.
+gUmidade <- gUmidade + facet_wrap(~ Estacao, nrow=1)
+# Exibe o gráfico
+gUmidade
+
+
+# gVento representa o gráfico das médias diárias de velocidade do vento em cada
+# estação, levando em consideração os 3 anos com registro.
+# Alem disso, traçamos uma curva de ajuste com intervalo de confiança para
+# termos uma referência da tendendencia dos valores desenhados ao longo do periodo
+
+# Cria o gráfico gVento indicando que os dados deverão ser agrupados por Estacao
+# e que cada estação deverá ter uma cor distinta, além disso, o eixo X deverá contem os
+# valores dos ordinais de cada dia dentro de sua estação.
+gVento <- ggplot(mediaPorDia, aes(x=diaEstacao, group = Estacao, colour = Estacao)) 
+# Adiciona uma linha conectando todos os pontos de velocidade média do vento
+gVento <- gVento + geom_line(aes(y = Vento))
+# Adiciona uma curva de ajuste com intervalo de confiança para os valores de velocidade média do vento
+gVento <- gVento + geom_smooth(aes(y = Vento))
+# Pede a separação do gráfico em imagens distintas, sendo uma para cada estação, alem disso
+# indica que as imagens devem estar concentradas em uma unica linha.
+gVento <- gVento + facet_wrap(~ Estacao, nrow=1)
+# Exibe o gráfico
+gVento
+
+
+# gSensacao representa o gráfico das médias diárias de sensação térmica em cada
+# estação, levando em consideração os 3 anos com registro.
+# Alem disso, traçamos uma curva de ajuste com intervalo de confiança para
+# termos uma referência da tendendencia dos valores desenhados ao longo do periodo.
+
+# Cria o gráfico gSensacao indicando que os dados deverão ser agrupados por Estacao
+# e que cada estação deverá ter uma cor distinta, além disso, o eixo X deverá contem os
+# valores dos ordinais de cada dia dentro de sua estação.
+gSensacao <- ggplot(mediaPorDia, aes(x=diaEstacao, group = Estacao, colour = Estacao)) 
+# Adiciona uma linha conectando todos os pontos de sensação térmica média
+gSensacao <- gSensacao + geom_line(aes(y = Sensacao))
+# Adiciona uma curva de ajuste com intervalo de confiança para os valores de sensação térmica média
+gSensacao <- gSensacao + geom_smooth(aes(y = Sensacao))
+# Pede a separação do gráfico em imagens distintas, sendo uma para cada estação, alem disso
+# indica que as imagens devem estar concentradas em uma unica linha.
+gSensacao <- gSensacao + facet_wrap(~ Estacao, nrow=1)
+# Exibe o gráfico
+gSensacao
 
